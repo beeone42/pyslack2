1b8f83b475e0d9d1df9df8153b0a6e823115d2bb
Sebastien BENOIT <sb@42.fr>
2016-06-15 11:28:55 +0200

    added my slack nickname in config[master] to receive errors by DM

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -5,6 +5,7 @@ import json, os, bot
 with open("config.json") as json_data_file:
     bot.config = json.load(json_data_file)
     API_TOKEN = bot.config["token"]
+    ERRORS_TO = bot.config["master"]
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 PLUGINS = ['help', 'cam', 'open', 'snap']
