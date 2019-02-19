5e4f3a0d0d4901f0fd5b12574e9412e00328dcd3
Sebastien BENOIT <sb@42.fr>
2019-02-19 15:33:40 +0100

    bank direct

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
 from slackbot.bot import listen_to
 from bot import *
-import re, requests
+import re, requests, json
 
 msg_help['balance'] = "*balance* <login> _# show user foodtruck balance_"
 msg_help['ops'] = "*ops* <login> _# show user foodtruck operations_"
@@ -10,24 +10,25 @@ msg_help['pay']     = "*pay* <login> <amount> _# remove amount from user foodtru
 @listen_to('^balance ([a-zA-Z0-9_-]+)$')
 def balance(message, login):
     if (my_chan(message)):
-        r = requests.get(config['bank_base'] + "balance.php?login=" + login + "&key=" + config['bank_key'])
-        message.send(r.content)
+        r = requests.get(config['bank_base'] + "balance.php?login=" + login + "&vendor=" + config['bank_vendor'] + "&key=" + config['bank_key'])
+        j = json.loads(r.content)
+        message.send(j['balance'])
 
 @listen_to('^ops ([a-zA-Z0-9_-]+)$')
-def balance(message, login):
+def ops(message, login):
     if (my_chan(message)):
-        r = requests.get(config['bank_base'] + "ops.php?login=" + login + "&key=" + config['bank_key'])
+        r = requests.get(config['bank_base'] + "ops.php?login=" + login + "&vendor=" + config['bank_vendor'] + "&key=" + config['bank_key'])
         message.send(r.content)
 
 @listen_to('^credit ([a-zA-Z0-9_-]+) ([0-9.]+)$')
 def credit(message, login, amount):
     if (my_chan(message)):
-        r = requests.get(config['bank_base'] + "credit.php?login=" + login + "&amount=" + amount + "&key=" + config['bank_key'])
+        r = requests.get(config['bank_base'] + "credit.php?login=" + login + "&amount=" + amount + "&vendor=" + config['bank_vendor'] + "&key=" + config['bank_key'])
         message.send(r.content)
 
 @listen_to('^pay ([a-zA-Z0-9_-]+) ([0-9.]+)$')
 def pay(message, login, amount):
     if (my_chan(message)):
-        r = requests.get(config['bank_base'] + "pay.php?login=" + login + "&amount=" + amount + "&key=" + config['bank_key'])
+        r = requests.get(config['bank_base'] + "pay.php?login=" + login + "&amount=" + amount + "&vendor=" + config['bank_vendor'] + "&key=" + config['bank_key'])
         message.send(r.content)
 
